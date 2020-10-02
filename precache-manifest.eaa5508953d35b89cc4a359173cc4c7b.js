d832a99447a34319ca7ffb3249a60dd9f5e51882
Jacob Krol <phone12.jk@gmail.com>
2020-10-02 00:40:03 -0500

    Updates

---
+++ precache-manifest.eaa5508953d35b89cc4a359173cc4c7b.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e8945451ed3c5754c4e8ac0c26c3570",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d562b947a04267b8a8e7",
+    "url": "/static/css/main.305a6f29.chunk.css"
+  },
+  {
+    "revision": "f9b0ab27ac0dc1159117",
+    "url": "/static/js/2.9ce2cd47.chunk.js"
+  },
+  {
+    "revision": "9958780d182350cee9cc1b612e70eb1e",
+    "url": "/static/js/2.9ce2cd47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d562b947a04267b8a8e7",
+    "url": "/static/js/main.86857ad2.chunk.js"
+  },
+  {
+    "revision": "32e77c81fc1344e4a90c",
+    "url": "/static/js/runtime-main.3e3320f2.js"
+  },
+  {
+    "revision": "ea46b22c5bffb143f7581f2599bbe41b",
+    "url": "/static/media/angels.ea46b22c.png"
+  },
+  {
+    "revision": "d1d9277d77facbb2554b3fe4d4fea3e2",
+    "url": "/static/media/astros.d1d9277d.png"
+  },
+  {
+    "revision": "bdba8bdcc3393be96b37da8bd388df95",
+    "url": "/static/media/bluejays.bdba8bdc.png"
+  },
+  {
+    "revision": "656cd78490d283467db562a5f422095d",
+    "url": "/static/media/brewers.656cd784.png"
+  },
+  {
+    "revision": "8f6bc667d2f9c34f1978e3ed29e8dac7",
+    "url": "/static/media/cardinals.8f6bc667.png"
+  },
+  {
+    "revision": "589577e128ec95c5271bbbb866445e40",
+    "url": "/static/media/cubs.589577e1.png"
+  },
+  {
+    "revision": "bfe4d2e7d0cb072ca8a7bb6c52826ca4",
+    "url": "/static/media/diamondbacks.bfe4d2e7.png"
+  },
+  {
+    "revision": "9e2e766d5239afa34090d7fb56f2d9e3",
+    "url": "/static/media/indians.9e2e766d.png"
+  },
+  {
+    "revision": "ab9e891a442c6b4802319bd3a47c622a",
+    "url": "/static/media/mariners.ab9e891a.png"
+  },
+  {
+    "revision": "0c35d225226eec029a067d095868fdd2",
+    "url": "/static/media/marlins.0c35d225.png"
+  },
+  {
+    "revision": "4fc6fea20de2dd7d6903ab7dea29a15d",
+    "url": "/static/media/nationals.4fc6fea2.png"
+  },
+  {
+    "revision": "de4b45fa05ef93bd70c2abaddc03c13c",
+    "url": "/static/media/orioles.de4b45fa.png"
+  }
+]);
